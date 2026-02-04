a1d7e11b8707bba772d13980bca98a2765c1991b
Cole Cianflone <colecianflone123@gmail.com>
2026-02-03 20:12:49 -0600

    Added Hit signal and registered OnBodyEntered event to the player

--- Player.cs
+++ Player.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Player : Area2D
 {
+    [Signal]
+    public delegate void HitEventHandler();
+
     [Export]
     public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
 
@@ -12,7 +15,14 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
+        // add events
+        Area2D player = GetNode<Area2D>("Player");
+        player.BodyEntered += OnBodyEntered;
+
+        // set screen size
         this.ScreenSize = GetViewportRect().Size;
+
+        // hide player at start
         this.Hide();
     }
 
@@ -70,5 +80,13 @@ public partial class Player : Area2D
             animatedSprite2D.Animation = "up";
             animatedSprite2D.FlipV = velocity.Y > 0;
         }
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        Hide(); // Player disappears after being hit.
+        EmitSignal(SignalName.Hit);
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
     }
 }
