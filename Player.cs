1a7b23b2e78e2de5931d11148baecd4301f0b5b6
Cole Cianflone <colecianflone123@gmail.com>
2026-02-03 20:14:17 -0600

    Squashed commit of the following:
    commit e07acff1de77f89d5ef5fcb4361ff6ce75e8e90b
    Author: Cole Cianflone <colecianflone123@gmail.com>
    Date:   Tue Feb 3 20:13:26 2026 -0600
    
        Added start function for new games
    
    commit a1d7e11b8707bba772d13980bca98a2765c1991b
    Author: Cole Cianflone <colecianflone123@gmail.com>
    Date:   Tue Feb 3 20:12:49 2026 -0600
    
        Added Hit signal and registered OnBodyEntered event to the player
    
    commit 62c3f2ef41f3cdfda9975c73886f22ec992f63a3
    Author: Cole Cianflone <colecianflone123@gmail.com>
    Date:   Tue Feb 3 20:06:26 2026 -0600
    
        Making player hidden at start of game
    
    commit 0f262fbd300b233503453bbfa3abbaea9225272f
    Author: Cole Cianflone <colecianflone123@gmail.com>
    Date:   Tue Feb 3 20:05:45 2026 -0600
    
        Adding vertical/horizontal flip for the animation

--- Player.cs
+++ Player.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Player : Area2D
 {
+    [Signal]
+    public delegate void HitEventHandler();
+
     [Export]
     public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
 
@@ -12,7 +15,15 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-		this.ScreenSize = GetViewportRect().Size;
+        // add events
+        Area2D player = GetNode<Area2D>("Player");
+        player.BodyEntered += OnBodyEntered;
+
+        // set screen size
+        this.ScreenSize = GetViewportRect().Size;
+
+        // hide player at start
+        this.Hide();
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -57,5 +68,32 @@ public partial class Player : Area2D
             x: Mathf.Clamp(this.Position.X, 0, this.ScreenSize.X),
             y: Mathf.Clamp(this.Position.Y, 0, this.ScreenSize.Y)
         );
+
+        if (velocity.X != 0)
+        {
+            animatedSprite2D.Animation = "walk";
+            animatedSprite2D.FlipV = false;
+            animatedSprite2D.FlipH = velocity.X < 0;
+        }
+        else if (velocity.Y != 0)
+        {
+            animatedSprite2D.Animation = "up";
+            animatedSprite2D.FlipV = velocity.Y > 0;
+        }
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        Hide(); // Player disappears after being hit.
+        EmitSignal(SignalName.Hit);
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
+
+    public void Start(Vector2 position)
+    {
+        Position = position;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
     }
 }
